ac8f2d35e7fe2c7466c2e13e7ef0fc38073182a2
Petar <petar.b.petkov@gmail.com>
2016-08-27 23:40:28 +0300

    Added menu with levels to choose

--- Anagrams/ViewController.swift
+++ Anagrams/ViewController.swift
@@ -19,8 +19,6 @@ class ViewController: UIViewController {
                             
     override func viewDidLoad() {
         super.viewDidLoad()
-        //load the model
-        let level1 = Level(levelNumber: 1)
         //create the view and add it to the main view
         let gameView = UIView(frame:CGRectMake(0,0,ScreenWidth,ScreenHeight))
         self.view.addSubview(gameView)
@@ -29,12 +27,15 @@ class ViewController: UIViewController {
         self.view.addSubview(hudView)
         
         controller.gameView = gameView
-        controller.level = level1;
         controller.hud = hudView
-        controller.dealRandomAnagram()
+
         
     }
-
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        self.showLevelMenu()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -42,6 +43,33 @@ class ViewController: UIViewController {
 
     override func prefersStatusBarHidden() -> Bool {
         return true
+    }
+    
+    func showLevelMenu() {
+        let alertController = UIAlertController(title:"Choose Difficulty Level", message: nil, preferredStyle: UIAlertControllerStyle.Alert)
+        
+        let easy = UIAlertAction(title: "Easy-peasy", style: .Default, handler: {(alert:UIAlertAction!) in
+            self.showLevel(1)
+            })
+        let hard = UIAlertAction(title: "Challenge accepted", style: .Default, handler: {(alert:UIAlertAction!) in
+            self.showLevel(2)
+        })
+        
+        let hardest = UIAlertAction(title: "I am totally hard-core", style: .Default, handler: {(alert:UIAlertAction!) in
+            self.showLevel(3)
+        })
+        
+        alertController.addAction(easy)
+        alertController.addAction(hard)
+        alertController.addAction(hardest)
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
+        
+    }
+    
+    func showLevel(levelNumber: Int){
+        controller.level = Level(levelNumber: levelNumber)
+        controller.dealRandomAnagram()
     }
 
 }
